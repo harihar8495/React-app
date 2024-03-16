216998ebd86eb595ee9ef85bfe919bd666389dce
harihar8495 <harithangavelu08@gmail.com>
2024-03-16 12:17:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import DataFetchingOne from './components/DataFetchingOne';
+import DataFetchingTwo from './components/DataFetchingTwo';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <DataFetchingOne /> */}
+      {/* <DataFetchingTwo /> */}
+      <Counter />
+         </div>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,33 @@
+import React, {useState, } from "react";
+
+function Counter() {
+
+    const[counterOne, setCounterOne] = useState(0)
+    const[counterTwo, setCounterTwo] = useState(0)
+
+    const incrementOne = () =>{
+        setCounterOne( counterOne + 1)
+    } 
+
+    const incrementTwo = ()=> {
+        setCounterTwo( counterTwo + 1)
+    }
+
+    const isEven = () => {
+        return counterOne % 2 === 0
+    }
+    return(
+        <div>
+            <div>
+                <button onClick={ incrementOne}>CounterOne - {counterOne}</button>
+                <span>{isEven() ? 'Even': 'Odd'}</span>
+            </div>
+            <div>
+                <button onClick={ incrementTwo}>CounterOne - {counterTwo}</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Counter;
